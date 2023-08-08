79d0c1a3e266e4e159f8c8344b8d11c0b02cebfb
Stepan Zabelin <stepanzabelin@gmail.com>
2023-08-08 18:28:54 +0200

    improved README and version mode

--- README.md
+++ README.md
@@ -1,9 +1,46 @@
-# Clirio example
+# Clirio Starter Kit
+
+**[Clirio](https://www.npmjs.com/package/clirio)** is a mini framework for node.js command-line interfaces based on TypeScript, decorators, DTOs
+
+This project is a ready-made Typescript assembly with Clirio. Code formatting rules, typescript building and unit tests are configured
+
+Clirio is [developed](https://github.com/stepanzabelin/clirio) according to SOLID principles, so it is possible apply OOP, dependency injection and other programming patterns.
+
+Clirio starter kit contains the recommended assembly. But it is possible to integrate any other libraries and custom decorators.
+
+## Get started
+
+Pull this project from git and create your own repo
+
+```bash
+$ git clone https://github.com/stepanzabelin/clirio-starter-kit my-cli-app
+$ cd my-cli-app
+$ rm -rf .git
+$ git init
+```
+
+Open `package.json`. Change the following fields: `name`, `version`, `description`, `repository`, `keywords`, `author`, `bin`
 
 ## Installing
 
 ```bash
 yarn install
+```
+
+## Run cli commands
+
+In development mode, commands work when running the following script
+
+```bash
+$ yarn dev {any command}
+```
+
+Examples of command-lines
+
+```bash
+$ yarn dev math formula "(2 + 5) ^ 2 - 7"
+$ yarn dev math sum 5 10
+$ yarn dev weather get --city=Prague
 ```
 
 ## Building
@@ -12,12 +49,6 @@ yarn install
 yarn build
 ```
 
-## Run cli commands
+## Calling CLI
 
-Test command-lines
-
-```bash
-$ yarn dev math formula "(2 + 5) ^ 2 - 7"
-$ yarn dev math sum 5 10
-$ yarn dev weather get --city=Prague
-```
+The `bin` param in `package.json` is responsible for calling CLI. Change it to your version before building the application and publishing your package
--- src/modules/common/actions/version-help/version-help.service.ts
+++ src/modules/common/actions/version-help/version-help.service.ts
@@ -1,18 +1,55 @@
 import { injectable } from 'tsyringe';
-import { readFile } from 'fs/promises';
-import { resolve } from 'path';
+import { readFile, lstat } from 'fs/promises';
+import path from 'path';
 import { ResultService } from '../../../../services';
+import { ClirioCommonError } from 'clirio';
 
 @injectable()
 export class VersionHelpService {
   constructor(private readonly resultService: ResultService) {}
 
   public async entry() {
-    const result = await readFile(
-      resolve(process.cwd(), './package.json'),
-      'utf8',
-    ).then((contents) => JSON.parse(contents));
+    const packagePath = await this.findPackageJson();
 
-    this.resultService.success(result.version);
+    if (!packagePath) {
+      throw new ClirioCommonError(`package.json not found`, {
+        errCode: 'BROKEN_CODE',
+      });
+    }
+
+    const contents = await readFile(packagePath, 'utf8');
+
+    const packageData = JSON.parse(contents);
+
+    this.resultService.success(packageData.version);
+  }
+
+  public async findPackageJson() {
+    const scriptDir = __dirname;
+    const maxPath = process.cwd();
+
+    let currentDir = scriptDir;
+
+    while (this.matchPath(currentDir, maxPath)) {
+      const potentialPackageJsonPath = path.join(currentDir, 'package.json');
+
+      const exists = await lstat(potentialPackageJsonPath)
+        .then(() => true)
+        .catch(() => false);
+
+      if (exists) {
+        return potentialPackageJsonPath;
+      }
+
+      currentDir = path.dirname(currentDir);
+    }
+
+    return null;
+  }
+
+  matchPath(currentDir: string, maxPath: string) {
+    const resolvedCurrentDir = path.resolve(currentDir);
+    const resolvedMaxPath = path.resolve(maxPath);
+    return resolvedCurrentDir.startsWith(resolvedMaxPath);
   }
 }
